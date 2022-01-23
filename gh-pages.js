6279fc9c2820cc80b56c879e9b3a0c96144e08c5
Darwin Manalo <darwinmanalophi@gmail.com>
2022-01-23 20:39:50 +0800

    Change branch for ghpages

--- gh-pages.js
+++ gh-pages.js
@@ -3,7 +3,7 @@ var ghpages = require("gh-pages");
 ghpages.publish(
   "public", // path to public directory
   {
-    branch: "gh-pages",
+    branch: "main",
     repo: "https://github.com/darwinphi/digitrivia.git",
     user: {
       name: "Darwin",
